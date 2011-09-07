907a9aa16c7750ed2d43f7868b5ee617c04aff8e
maximilianou <maximilianou@gmail.com>
2011-09-07 10:43:56 -0300

    Inicializacion de Clases

---
+++ src/simple/DbSimple.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple;
+
+/**
+ *
+ * @author maxi
+ */
+public class DbSimple {
+  
+    public static void main(String[]args){
+        
+    }
+    
+}
