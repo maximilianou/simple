c76418f2b5d2d8865a54031edb863253cc1561a4
maximilianou <maximilianou@debian1>
2011-11-05 18:44:01 -0300

    Agregando CrearJavaProgramador

---
+++ crearjavaprogramador/CrearJavaProgramador.java
@@ -0,0 +1,8 @@
+package simple.crearjavaprogramador;
+
+public class CrearJavaProgramador {
+
+    public static void main(String[] args) {
+        System.out.println("Crear Buenos Aires. CrearJavaProgramador");
+    }
+}
