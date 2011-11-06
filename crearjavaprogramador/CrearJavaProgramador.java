f7162681924da8d08af1b0b59f318e7a7b27965a
maximilianou <maximilianou@gmail.com>
2011-11-05 21:33:59 -0300

    Agregados algunos TODO

--- crearjavaprogramador/CrearJavaProgramador.java
+++ crearjavaprogramador/CrearJavaProgramador.java
@@ -6,7 +6,19 @@ public class CrearJavaProgramador {
         System.out.println("Crear Buenos Aires. CrearJavaProgramador");
 
         Producto tabletpc = new Producto();
-        
+//TODO: Objeto	( El codigo fuente es archivo de texto. Mercurial )
+//TODO: Atributo 	( tipos basicos )
+//TODO: Metodo	( que puede hacer esta clase, return )
+//TODO: Interface, web, html	( jetty )
+//TODO: Encapsulamiento 	( preparandose para el cambio )
+//TODO: Excepciones	( consistencia vs inconsistencia, el sistema no se cae )
+//TODO: Herencia	( reutilizando codigo, familia )
+//TODO: Abstracto	( luchando contra la inconsistencia )
+//TODO: Collections	
+//TODO: Streams	( File, FileReader, BufferedReader, Traductor, URL, HTTPConnection )
+//TODO: JDBC	( hsqldb )
+//TODO: Configuration ( Singleton, Cache )
+//TODO: Pool Objetos
         
         System.out.println("Crear Buenos Aires. CrearJavaProgramador");
     }
