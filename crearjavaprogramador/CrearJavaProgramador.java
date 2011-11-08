517005fc46294ee06d7531abe04fe020fa503b1e
maximilianou <maximilianou@localhost>
2011-11-08 19:38:32 -0300

    Aclaraciones

--- crearjavaprogramador/CrearJavaProgramador.java
+++ crearjavaprogramador/CrearJavaProgramador.java
@@ -8,17 +8,29 @@ public class CrearJavaProgramador {
         Producto tabletpc = new Producto();
 
 //TODO: Objeto	( El codigo fuente es archivo de texto. Mercurial )
+
 //TODO: Atributo 	( tipos basicos )
+
 //TODO: Metodo	( que puede hacer esta clase, return )
-//TODO: Interface, web, html	( jetty )
+
+//TODO: Interface, web, html	( jetty, toString(), toHtml(), toJson()  )
+
 //TODO: Encapsulamiento 	( preparandose para el cambio )
+
 //TODO: Excepciones	( consistencia vs inconsistencia, el sistema no se cae )
+
 //TODO: Herencia	( reutilizando codigo, familia )
+
 //TODO: Abstracto	( luchando contra la inconsistencia )
-//TODO: Collections	
-//TODO: Streams	( File, FileReader, BufferedReader, Traductor, URL, HTTPConnection )
+
+//TODO: Collections
+
+//TODO: Streams	( File, FileReader, BufferedReader, Traductor, URL, HTTPConnection, Properties )
+
 //TODO: JDBC	( hsqldb )
+
 //TODO: Configuration ( Singleton, Cache )
+
 //TODO: Pool Objetos
         
         System.out.println("Crear Buenos Aires. CrearJavaProgramador Exito!!");
