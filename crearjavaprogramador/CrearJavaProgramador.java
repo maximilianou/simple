c9e25c81101bd11e2199aedbf3e820f2ff8e9250
maximilianou <maximilianou@debian1>
2011-11-12 09:59:43 -0300

    Programador, replanificacion temas

--- crearjavaprogramador/CrearJavaProgramador.java
+++ crearjavaprogramador/CrearJavaProgramador.java
@@ -5,30 +5,46 @@ public class CrearJavaProgramador {
     public static void main(String[] args) {
         System.out.println("Crear Buenos Aires. CrearJavaProgramador");
 
-        Producto tabletpc = new Producto();
+// Java Core Objetos I
+//TODO: Objetos ( El codigo fuente es archivo de texto. Mercurial )
 
-//TODO: Objeto	( El codigo fuente es archivo de texto. Mercurial )
+        Producto tabletpcA = new Producto();
+        Producto tabletpcB = new Producto();
 
-//TODO: Atributo 	( tipos basicos )
+//TODO: Atributo ( tipos basicos )
 
-//TODO: Metodo	( que puede hacer esta clase, return )
+        tabletpcA.nombre = "Samsung Galaxy Tab 10";
+        tabletpcA.descripcion = "1GHz dual-core NVIDIA Tegra 2 processor, 10.1inch widescreen 1280x800 149ppi";
+        tabletpcA.precio = 500;
+        
+        tabletpcB.nombre = "IPad2";
+        tabletpcB.descripcion = "1GHz Apple A5 SoC, 9.7inch 1024×768 132ppi";
+        tabletpcB.precio = 500;
+        
+        
+//TODO: Metodo ( que puede hacer esta clase, return )
 
-//TODO: Interface, web, html	( jetty, toString(), toHtml(), toJson()  )
+//TODO: Encapsulamiento  ( preparandose para el cambio )
 
-//TODO: Encapsulamiento 	( preparandose para el cambio )
+//TODO: Excepciones ( consistencia vs inconsistencia, el sistema no se cae )
 
-//TODO: Excepciones	( consistencia vs inconsistencia, el sistema no se cae )
+// Java Core Objetos II
+//TODO: Herencia ( reutilizando codigo, familia )
 
-//TODO: Herencia	( reutilizando codigo, familia )
+//TODO: Abstracto ( luchando contra la inconsistencia )
 
-//TODO: Abstracto	( luchando contra la inconsistencia )
-
+//TODO: Interface, web, html ( toString(), toHtml(), toJson()  )
+        
+// Java Api I Memoria
 //TODO: Collections
 
+// Java Api II Comunicacion
 //TODO: Streams	( File, FileReader, BufferedReader, Traductor, URL, HTTPConnection, Properties )
 
-//TODO: JDBC	( hsqldb )
+// Java Api III SQL
+//TODO: JDBC ( hsqldb )
 
+// Java Patrones I
 //TODO: Configuration ( Singleton, Cache )
 
 //TODO: Pool Objetos
--- crearjavaprogramador/Producto.java
+++ crearjavaprogramador/Producto.java
@@ -1,5 +1,7 @@
 package simple.crearjavaprogramador;
 
 public class Producto {
-    
+    String nombre;
+    String descripcion;
+    double precio;
 }
