5a0d69080d1e1278417f3e51f8968a80f1120813
maximilianou <maximilianou@gmail.com>
2011-11-17 16:27:03 -0300

    Agregando Validator

--- crearjavaprogramador/entidades/Validator.java
+++ crearjavaprogramador/entidades/Validator.java
@@ -1,5 +1,10 @@
 package simple.crearjavaprogramador.entidades;
+//maximilianou@gmail.com
 
 public class Validator {
-    
+    public static void positive(double pos) throws BelowZeroException{
+        if( pos < 0 ){
+            throw new  BelowZeroException("Verifique que el valor sea Mayor a Cero.");
+        }
+    }
 }
