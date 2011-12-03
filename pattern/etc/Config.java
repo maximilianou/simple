fd99257bad0c8da40656d52c1f867ac80813615a
maximilianou <maximilianou@gmail.com>
2011-12-02 21:26:30 -0300

    Pattern Singleton Timer

--- pattern/etc/Config.java
+++ pattern/etc/Config.java
@@ -10,4 +10,13 @@ public class Config {
         }
         return instance;
     }
+    
+    
+    
+    public static void main(String[]args){
+        System.out.println( Config.getInstance() );
+        System.out.println( Config.getInstance() );
+        System.out.println( Config.getInstance() );
+        System.out.println( Config.getInstance() );
+    }
 }
