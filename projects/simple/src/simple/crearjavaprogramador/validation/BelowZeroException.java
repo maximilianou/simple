43ecbc3c75003cb18c5bd7db08283ab064f528a4
maximilianou <maximilianou@gmail.com>
2011-12-15 08:30:08 -0300

    regenerando, borrado, anterior

---
+++ projects/simple/src/simple/crearjavaprogramador/validation/BelowZeroException.java
@@ -0,0 +1,10 @@
+package simple.crearjavaprogramador.validation;
+//maximilianou@gmail.com
+
+public class BelowZeroException extends Exception {
+
+    public BelowZeroException(String msg){
+        super(msg);
+    }
+    
+}
