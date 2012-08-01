c82b447454c91cab3ce1cb81a3e1ee5febafc116
maximilianou <maximilianou@gmail.com>
2012-07-31 22:16:46 -0300

    Ejemplos.. Patrones Thread Producer Consumer

---
+++ src/java/pattern/behavior/a/ObserverTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pattern.behavior.a;
+
+/**
+ *
+ * @author maximilianou
+ */
+public class ObserverTest {
+    
+}
