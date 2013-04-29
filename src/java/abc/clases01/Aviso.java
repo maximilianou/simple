b0070d30f7a1daeb5625ffdaf5857708dcf76104
maximilianou <maximilianou@gmail.com>
2013-04-29 20:00:28 -0300

    abc.clases01

---
+++ src/java/abc/clases01/Aviso.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abc.clases01;
+
+/**
+ *
+ * @author maximilianou
+ */
+public class Aviso {
+    private String titulo;
+    private String descripcion;
+    private float precio;
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the precio
+     */
+    public float getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+}
