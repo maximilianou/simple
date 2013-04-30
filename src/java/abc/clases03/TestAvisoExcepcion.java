20d50fb51e4b66600cfe7edb983535ed44af931b
maximilianou <maximilianou@cocina>
2013-04-29 22:43:48 -0300

    abc.clases03 Colecciones

--- src/java/abc/clases03/TestAvisoExcepcion.java
+++ src/java/abc/clases03/TestAvisoExcepcion.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package abc.clases03.test;
+package abc.clases03;
 
 import abc.clases03.Aviso;
 import java.util.logging.Level;
---
+++ src/java/abc/clases04/TestAvisoColecciones.java
@@ -0,0 +1,64 @@
+package abc.clases04;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author maximilianou
+ */
+public class TestAvisoColecciones {
+
+    public static void main(String[] args) {
+        System.out.println("[..] " + TestAvisoColecciones.class.getCanonicalName() + " --------");
+
+        ArrayList<Aviso> listaAvisos = new ArrayList();
+        System.out.println("Lista Size Cero: " + listaAvisos.size());
+
+        System.out.println(" -- ------------------------------------------ -- ");
+        System.out.println(" -- Coleccion de Objetos -- ");
+        try {
+            listaAvisos.add(new Aviso("Peras", 15, "Amarillas for Export"));
+            System.out.println(listaAvisos);
+            System.out.println("Lista Size Uno: " + listaAvisos.size());
+            listaAvisos.add(new Aviso("Manzana", 20, "Verdes Grandes"));
+            System.out.println(listaAvisos);
+            listaAvisos.add(new Aviso("Lechuga", 10, "Criolla para el asadito"));
+            System.out.println(listaAvisos);
+            System.out.println("Lista Size Tres: " + listaAvisos.size());
+            System.out.println("[OK] Coleccion de objetos cargada");
+
+            System.out.println(" -- --- -- ");
+            System.out.println(" -- Una forma de recorrer la Coleccion, Lista -- ");
+            for (Aviso avisoActual : listaAvisos) {
+                System.out.println(" : " + avisoActual + " : ");
+            }
+            System.out.println(" -- --- -- ");
+            System.out.println(" -- Otra forma de recorrer la Coleccion, Lista -- ");
+            for (int i = 0; i < listaAvisos.size(); i++) {
+                Aviso avisoEnIndiceI = listaAvisos.get(i);
+                System.out.println(" : " + i + " : " + avisoEnIndiceI + " : ");
+            }
+
+            System.out.println(" -- --- -- ");
+
+            System.out.println(" -- Vaciamos la Coleccion, Lista -- ");
+
+            listaAvisos.clear();
+            System.out.println("Lista Size Cero: " + listaAvisos.size());
+            System.out.println(listaAvisos);
+ 
+            System.out.println(" -- --- -- ");
+        } catch (Exception ex) {
+            System.out.println("[ERROR] Hubo un error en la coleccion");
+            ex.printStackTrace();
+            Logger.getLogger(TestAvisoColecciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(" -- ------------------------------------------ -- ");
+
+
+        System.out.println("[Ok] " + TestAvisoColecciones.class.getCanonicalName() + " --------");
+
+    }
+}
