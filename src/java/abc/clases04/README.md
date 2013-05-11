9958a5085247d7ffb0195cd62ac8bfc4ced12505
maximilianou <maximilianou@cocina>
2013-05-11 14:36:28 -0300

    Export

---
+++ src/java/abc/clases04/README.md
@@ -0,0 +1,10 @@
+##  Lista Coleccion, Cargar, recorrer y Vaciar.
+-----
+>      abc.clases04.Aviso.java
+>      abc.clases04.TestAvisoColeccion.java
+
+* Ejemplo java una coleccion, una lista particularmente.
+* Ejemplo java de como llenar una coleccion.
+* Ejemplo java de como recorrer una coleccion.
+* Ejemplo java de como vaciar una coleccion.
+
---
+++ src/java/util/Readme.java
@@ -0,0 +1,88 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author maximilianou
+ */
+public class Readme {
+
+    String salida = "README.md";
+    String[] archivos = {
+        "src/java/abc/clases01/README.md",
+        "src/java/abc/clases01/Aviso.java",
+        "src/java/abc/clases01/TestAviso.java",
+        "src/java/abc/clases02/README.md",
+        "src/java/abc/clases02/Aviso.java",
+        "src/java/abc/clases02/TestAvisoConstructores.java",
+        "src/java/abc/clases03/README.md",
+        "src/java/abc/clases04/README.md"
+    };
+    FileWriter arch = null;
+    BufferedWriter barch = null;
+
+    public void inicializar() throws IOException {
+        File fs = new File(salida);
+        fs.delete();
+        arch = new FileWriter(salida, true);
+        barch = new BufferedWriter(arch);
+    }
+
+    public void exportar() {
+        for (String s : archivos) {
+            String p = "";
+            if (!s.endsWith(salida)) {
+                p = ">   ";
+            }
+            try {
+                exportar(s, p);
+            } catch (Exception ex) {
+                Logger.getLogger(Readme.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("[ERROR]Readme " + ex.getMessage() + s);
+
+            }
+        }
+        try {
+            barch.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Readme.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            arch.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Readme.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void exportar(String archName, String pref) throws FileNotFoundException, IOException {
+        FileReader in = new FileReader(archName);
+        BufferedReader bin = new BufferedReader(in);
+        while (bin.ready()) {
+            barch.write(pref + bin.readLine());
+            barch.newLine();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[..]Readme");
+        Readme r = new Readme();
+        try {
+            r.inicializar();
+            r.exportar();
+        } catch (IOException ex) {
+            Logger.getLogger(Readme.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("[ERROR]Readme");
+        }
+        System.out.println("[ok]Readme");
+    }
+}
