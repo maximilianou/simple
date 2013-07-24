9480ff45b60aaad56404debc79c03be43b1af6dd
maximilianou <maximilianou@gmail.com>
2013-07-24 08:31:55 -0300

    Diccionario Avisos

---
+++ src/java/abc/clases05/README.md
@@ -0,0 +1,10 @@
+##  Diccionario, categorizacion, Indice.
+-----
+* Ejemplo java un diccionario (clave, valor).
+* Ejemplo java indice por Categoria de lista de Avisos.
+
+>      abc.clases05.Aviso.java
+>      abc.clases05.Categoria.java
+>      abc.clases05.AdministradorAvisos.java
+>      abc.clases05.TestAvisoDiccionario.java
+
--- src/java/abc/clases05/TestAvisoDiccionario.java
+++ src/java/abc/clases05/TestAvisoDiccionario.java
@@ -1,21 +1,50 @@
 package abc.clases05;
-
-
-/**
- *
- * @author maximilianou
- */
+import java.util.*;
+/** @author maximilianou */
 public class TestAvisoDiccionario {
 
     public static void main(String[] args) {
         System.out.println("[..] " + TestAvisoDiccionario.class.getCanonicalName() + " --------");
 
-
         System.out.println(" -- ------------------------------------------ -- ");
         System.out.println(" -- Diccionario de Objetos, Singleton -- ");
-        
-        System.out.println(" -- ------------------------------------------ -- ");
+        Categoria cate1 = new Categoria("Educativo");
+        Categoria cate2 = new Categoria("Consultoria");
+        Categoria cate3 = new Categoria("Marketing");
+        try {
+            Aviso a1 = new Aviso("Curso Java", 300, cate1, "");
+            Aviso a9 = new Aviso("Curso JavaScript", 250, cate1, "");
+            Aviso a5 = new Aviso("Curso HTML5", 240, cate1, "");
+            Aviso a4 = new Aviso("Proyecto Actualizacion Tecnica", 30000, cate2, "");
+            Aviso a7 = new Aviso("Proyecto Infraestructura", 250000, cate2, "");
+            Aviso a8 = new Aviso("Proyecto Integracion", 50000, cate2, "");
+            Aviso a2 = new Aviso("Comunidades Sociales", 30000, cate3, "");
+            Aviso a6 = new Aviso("Avisos Pagos", 250000, cate3, "");
+            Aviso a3 = new Aviso("Integracion Empresas", 50000, cate3, "");
 
+            AdministradorAvisos.getInstance().addByCategory(a7);
+            AdministradorAvisos.getInstance().addByCategory(a8);
+            AdministradorAvisos.getInstance().addByCategory(a9);
+            AdministradorAvisos.getInstance().addByCategory(a1);
+            AdministradorAvisos.getInstance().addByCategory(a2);
+            AdministradorAvisos.getInstance().addByCategory(a3);
+            AdministradorAvisos.getInstance().addByCategory(a4);
+            AdministradorAvisos.getInstance().addByCategory(a5);
+            AdministradorAvisos.getInstance().addByCategory(a6);
+
+            Iterator<String> categoriasNombres 
+                    = AdministradorAvisos.getInstance().getCategoryNames();
+            while(categoriasNombres.hasNext()){
+              System.out.println("....Categoria...................");
+              System.out.println( 
+                      AdministradorAvisos.getInstance()
+                         .getAvisosByCategory(categoriasNombres.next()) );
+            }
+            
+        } catch (Exception e) {
+            System.out.println("[ER] " + e.getMessage());
+        }
+        System.out.println(" -- ------------------------------------------ -- ");
 
         System.out.println("[Ok] " + TestAvisoDiccionario.class.getCanonicalName() + " --------");
 
