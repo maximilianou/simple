bbefabbffab34dc26401386540e9e7c29f0d54d3
maximilianou <maximilianou@gmail.com>
2013-07-25 20:37:07 -0300

    RandomAccesFile

--- src/java/io/db/TestArchivoAccesoDirecto.java
+++ src/java/io/db/TestArchivoAccesoDirecto.java
@@ -1,16 +1,14 @@
 package io.db;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 /**
  * @author maximilianou
  */
-public class TestIndice {
+public class TestArchivoAccesoDirecto {
 
     public static void main(String[] args) {
         System.out.println("[....] TestIndice");
@@ -19,21 +17,16 @@ public class TestIndice {
         lista.add("03@cientouno@doscientos");
         lista.add("05@cuarenta@ocho");
         lista.add("08@siete@setentaytres");
-
         System.out.println("[....] TestIndice escribir -------- ");
         ArchivoAccesoDirecto.escribir(lista, "base.txt");
         System.out.println("[ OK ] TestIndice escribir ~~~~~~~~ ");
-
         ArrayList listaRespuesta = new ArrayList();
-
         System.out.println("[....] TestIndice escribir -------- ");
         ArchivoAccesoDirecto.leer("base.txt", listaRespuesta);
         System.out.println("[ OK ] TestIndice escribir ~~~~~~~~ ");
-        
         for (Object s : listaRespuesta) {
             System.out.println("[" + s + "]");
         }
-
         System.out.println("[ OK ] TestIndice");
     }
 }
