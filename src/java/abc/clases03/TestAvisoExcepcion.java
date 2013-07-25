33f087c40e50a8038cb80d2a0b64b8b1a041d121
maximilianou <maximilianou@living>
2013-07-25 20:25:35 -0300

    The MIT License

--- src/java/abc/clases03/TestAvisoExcepcion.java
+++ src/java/abc/clases03/TestAvisoExcepcion.java
@@ -9,10 +9,8 @@ public class TestAvisoExcepcion {
 
     public static void main(String[] args) {
         System.out.println("[....]TestAvisoExcepcion --------");
-
         System.out.println(" -- ------------------------------------------ -- ");
         System.out.println(" -- Instancias u Objetos de la clase Aviso -- ");
-
         Aviso avisoTres;
         try {
             // Respeta el precio positivo.
@@ -20,37 +18,29 @@ public class TestAvisoExcepcion {
             System.out.println(avisoTres.getTitulo());
             System.out.println(avisoTres.getPrecio());
             System.out.println(avisoTres.getDescripcion());
+            System.out.println(" -- [Ok]: Test Correcto, datos validos. -- ");
         } catch (Exception ex) {
-            Logger.getLogger(TestAvisoExcepcion.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(" -- [Error]: Test Correcto por Validacion -- ");
         }
-
-        System.out.println(" -- ---------------------------------------- -- ");
-
         System.out.println(" -- ------------------------------------------ -- ");
         System.out.println(" -- Instancias u Objetos de la clase Aviso -- ");
-
         Aviso avisoCuatro;
         try {
-            // Respeta el precio positivo.
+            // NO Respeta el precio positivo.
             avisoCuatro = new Aviso("Long Board", -10, "Ruedas de silicona, ideal para competencias.");
             System.out.println(avisoCuatro.getTitulo());
             System.out.println(avisoCuatro.getPrecio());
             System.out.println(avisoCuatro.getDescripcion());
-            System.out.println(" -- [ERROR]: Test Fallido -- ");
+            System.out.println(" -- [ERROR]: Test Fallido, datos invalidos -- ");
         } catch (Exception ex) {
             System.out.println(" -- [Ok]: Test Correcto por Validacion -- ");
             //ex.printStackTrace();
-            //Logger.getLogger(TestAvisoExcepcion.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        System.out.println(" -- ---------------------------------------- -- ");
-
         System.out.println(" -- ------------------------------------------ -- ");
         System.out.println(" -- Instancias u Objetos de la clase Aviso -- ");
-
         Aviso avisoCinco;
         try {
-            // Respeta el precio positivo.
+            // NO Respeta el titulo. No puede ser null
             avisoCinco = new Aviso(null, 4444, "Ruedas de silicona, ideal para competencias.");
             System.out.println(avisoCinco.getTitulo());
             System.out.println(avisoCinco.getPrecio());
@@ -59,17 +49,12 @@ public class TestAvisoExcepcion {
         } catch (Exception ex) {
             System.out.println(" -- [Ok]: Test Correcto por Validacion -- ");
             //ex.printStackTrace();
-            //Logger.getLogger(TestAvisoExcepcion.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        System.out.println(" -- ---------------------------------------- -- ");
-
         System.out.println(" -- ------------------------------------------ -- ");
         System.out.println(" -- Instancias u Objetos de la clase Aviso -- ");
-
         Aviso avisoSeis;
         try {
-            // Respeta el precio positivo.
+            // NO Respeta el titulo. Tiene que tener un titulo legible.
             avisoSeis = new Aviso("", 4444, "Ruedas de silicona, ideal para competencias.");
             System.out.println(avisoSeis.getTitulo());
             System.out.println(avisoSeis.getPrecio());
@@ -78,12 +63,8 @@ public class TestAvisoExcepcion {
         } catch (Exception ex) {
             System.out.println(" -- [Ok]: Test Correcto por Validacion -- ");
             //ex.printStackTrace();
-            //Logger.getLogger(TestAvisoExcepcion.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         System.out.println(" -- ---------------------------------------- -- ");
-
-
         System.out.println("[ OK ]TestAvisoExcepcion --------");
     }
 }
